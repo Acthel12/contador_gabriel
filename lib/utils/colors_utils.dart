2cf4d500e306b0e6bc9645529784bcd64c4a6856
Acthel12 <160182015+Acthel12@users.noreply.github.com>
2026-05-31 13:31:44 -0400

    feat(colors): añadir clase ColorsUtils con la paleta de colores  a usar

---
+++ lib/utils/colors_utils.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorsUtils {
+  static const Color red = Color.fromRGBO(255, 0, 69, 100);
+  static const Color grey = Color.fromRGBO(236, 167, 189, 100);
+  static const Color purple = Color.fromRGBO(6, 5, 59, 100);
+  static const Color pink = Color.fromRGBO(217, 36, 213, 100);
+}
